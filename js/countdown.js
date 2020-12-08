36e198eb9302110c735a1171f394e5430e82519f
astrobilkent <72880192+astrobilkent@users.noreply.github.com>
2020-12-08 11:39:56 +0300

    Add files via upload

---
+++ js/countdown.js
@@ -0,0 +1,31 @@
+
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('December 10, 2020 13:30:00').getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+      //do something later when date is reached
+      if (distance < 0) {
+
+       clearInterval(x);
+       document.getElementById('head').innerText = "Next event will be announced shortly";
+       document.getElementById('about-text').innerText = "Next event will be announced shortly";
+       document.getElementById('days').innerText = 0,
+         document.getElementById('hours').innerText = 0,
+         document.getElementById('minutes').innerText = 0,
+         document.getElementById('seconds').innerText = 0;
+      //  'IT'S Saturn timeee!;
+      }
+
+    }, second)
