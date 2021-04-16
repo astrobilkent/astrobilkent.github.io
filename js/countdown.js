fe0d61bbf691943d91cbd57ad0c12c4904574b8c
astrobilkent <72880192+astrobilkent@users.noreply.github.com>
2021-04-16 16:15:07 +0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -4,7 +4,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('December 10, 2020 13:30:00').getTime(),
+let countDown = new Date('April 24, 2021 21:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
